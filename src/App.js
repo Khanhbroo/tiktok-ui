ea2d3b0c1483d93a0d615329610a9f55a6a00efa
Khanhbroo <khanhmathias532211@gmail.com>
2025-02-11 18:37:50 +0700

    Build Base Layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/components/Layout';
 
-function App() {
+const App = () => {
     return (
         <Router>
             <div className="App">
@@ -34,6 +34,6 @@ function App() {
             </div>
         </Router>
     );
-}
+};
 
 export default App;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,15 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+// Bind to help us use snake case in module.scss
+const cx = classNames.bind(styles);
+
 const Header = () => {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('content')}></div>
+        </header>
+    );
 };
 
 export default Header;
