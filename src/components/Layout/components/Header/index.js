3e376818fef71a70d8a6192df33fc912d01790fb
Khanhbroo <khanhmathias532211@gmail.com>
2025-02-12 12:49:34 +0700

    UI Header Upgrading

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,12 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
+import image from '~/assets/images';
 
 // Bind to help us use snake case in module.scss
 const cx = classNames.bind(styles);
@@ -7,7 +14,26 @@ const cx = classNames.bind(styles);
 const Header = () => {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('content')}></div>
+            <div className={cx('content')}>
+                <img src={image.logo} alt="Tiktok"></img>
+                <div className={cx('search')}>
+                    <input
+                        placeholder="Search account and videos"
+                        spellCheck="false"
+                    />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon
+                        className={cx('loading')}
+                        icon={faSpinner}
+                    />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 };
